92577b527ee5eb483f34db7f4f6848660bfcf501
Maxim <bhd.m@ya.ru>
2015-10-23 18:02:14 +0300

    u

--- src/AppBundle/Controller/ProjectController.php
+++ src/AppBundle/Controller/ProjectController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -10,6 +11,7 @@ class ProjectController extends Controller
 {
     /**
      * @Route("/project/list", name="project_list")
+     * @Template("AppBundle:Project:list.html.twig")
      */
     public function listAction(Request $request){
         $projects = $this->getDoctrine()->getRepository('AppBundle:Project')->findBy(['enabled' => true]);
