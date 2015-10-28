f8a940b432fc64a370c67030e9708d41d0a7b142
Maxim <bhd.m@ya.ru>
2015-10-28 13:15:27 +0300

    update bower & update show tasks

---
+++ src/AppBundle/Resources/public/js/layout.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $.datetimepicker.setLocale('ru');
+    $('.datetimepicker').datetimepicker({
+        i18n:{
+            de:{
+                months:[
+                    'Январь','Февраль','Март','Апрель',
+                    'Май','Июнь','Июль','Август',
+                    'Сентябрь','Октябрь','Ноябрь','Декабрь'
+                ],
+                dayOfWeek:[
+                    "Пн", "Вт", "Ср", "Чт",
+                    "Пт", "Сб", "Вс"
+                ]
+            }
+        },
+        timepicker:false,
+        format:'d.m.Y'
+    });
+});
